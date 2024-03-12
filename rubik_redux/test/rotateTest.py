5f528405dd44e5cab71940f51433a9eba8c765d7
Alviere <againes0501@gmail.com>
2024-03-11 19:18:20 -0700

    Missing/invalid cube tests for rotate

--- rubik_redux/test/rotateTest.py
+++ rubik_redux/test/rotateTest.py
@@ -2,8 +2,121 @@ import unittest
 from rubik_redux.view.rotate import rotate
 
 class RotateTest(unittest.TestCase):
-    # Invalid cube tests
-    pass
+    # Invalid (or missing) cube tests
+    def test_missing_cube_parameter(self):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{})
+        self.assertEqual("Error: Input data: Expected parameter \"cube\"", str(result.exception))
+
+    def test_invalid_input_type_int(self):
+        with self.assertRaises(TypeError) as result:
+            rotate(**{'cube':3241})
+        self.assertEqual(f"Error: Input type: Expected string but recieved {type(1)}", str(result.exception))
+
+    def test_invalid_character_count_low(self):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':"aaaaaaaaabbbbbbbbbcccccccccdddddddddeeeeeeeeeffffffff"})
+        self.assertEqual('Error: Cube string length: Expected 54 characters but recieved 53', str(result.exception))
+
+    def test_invalid_character_count_high(self):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':"aaaaaaaaabbbbbbbbbcccccccccdddddddddeeeeeeeeeffffffffff"})
+        self.assertEqual('Error: Cube string length: Expected 54 characters but recieved 55', str(result.exception))
+
+    def test_invalid_characters_present(self):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':"=========---------!!!!!!!!!;;;;;;;;;/////////?????????"})
+        self.assertEqual('Error: Cube string contents: Please only use alphanumeric characters a-z, A-Z, 0-9', str(result.exception))
+
+    def test_invalid_unique_centerpoints(self):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':"aaaaaaaaabbbbbbbbbcccccccccdddddddddeeeeefeeeffffeffff"})
+        self.assertEqual('Error: Cube string contents: Centerpoints contain duplicates', str(result.exception))
+
+    def test_invalid_piece_match_centers(self):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':"aaaaaaaaabbbbbbbbbcccccccccdddddddddeeeeeeeeeffffffffg"})
+        self.assertEqual('Error: Cube string contents: Non-center pieces need to match present centerpieces', str(result.exception))
+
+    def test_invalid_piece_counts(self):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':"aaaaaaaaabbbbbbbbbcccccccccdddddddddeeeeeeeeeffffffffa"})
+        self.assertEqual('Error: Cube string contents: Require 9 pieces of each symbol', str(result.exception))
+
+    
+    # Tests for unsolvable cubes
+    def unsolvable_impossible_piece_test(self, cube_string):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':cube_string})
+        self.assertEqual('Error: Cube unsolvable: Impossible pieces', str(result.exception))
+    
+    def test_unsolvable_impossible_piece_1(self): # edges with same colour on both sides
+        self.unsolvable_impossible_piece_test("bwbbbbbbbooooooooogggggggggrrrrrbrrrwwwrwwwwwyyyyyyyyy")
+    def test_unsolvable_impossible_piece_2(self): # edges with colours of opposite cube sides
+        self.unsolvable_impossible_piece_test("bbbobbbbbooogooooogggrgggggrrrbrrrrrwwwwwwwwwyyyyyyyyy")
+    def test_unsolvable_impossible_piece_3(self): # corner with improper colour amounts
+        self.unsolvable_impossible_piece_test("bbwbbbrbbooooooooogggggggggrrbrrrrrrwwwwwwbwwyyyyyyyyy")
+    def test_unsolvable_impossible_piece_4(self): # corners with colours of opposite cube sides
+        self.unsolvable_impossible_piece_test("obbbbbbbbgoooooooorggggggggbrrrrrrrrwwwwwwwwwyyyyyyyyy")
+
+
+    def unsolvable_edge_parity_test(self, cube_string):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':cube_string})
+        self.assertEqual('Error: Cube unsolvable: Edge parity', str(result.exception))
+
+    def test_unsolvable_edge_parity_1(self):
+        self.unsolvable_edge_parity_test("bwbbbbbbbooooooooogggggggggrrrrrrrrrwwwwwwwbwyyyyyyyyy")
+    def test_unsolvable_edge_parity_2(self):
+        self.unsolvable_edge_parity_test("wbrybrbwggoyyooyywbowgggrwbbbgrrorryowrrwboyyogowygwbg")
+    def test_unsolvable_edge_parity_3(self):
+        self.unsolvable_edge_parity_test("wyrybybwggoyrooyywbowgggrwbbbgrrorryowrrwbobyogowygwbg")
+    
+
+    def unsolvable_corner_parity_test(self, cube_string):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':cube_string})
+        self.assertEqual('Error: Cube unsolvable: Corner parity', str(result.exception))
+
+    def test_unsolvable_corner_parity_1(self):
+        self.unsolvable_corner_parity_test("bbobbbbbbwoooooooogggggggggrrrrrrrrrwwwwwwwwbyyyyyyyyy")
+    def test_unsolvable_corner_parity_2(self):
+        self.unsolvable_corner_parity_test("bbwbbbbbbboooooooogggggggggrrrrrrrrrwwwwwwwwoyyyyyyyyy")
+    def test_unsolvable_corner_parity_3(self):
+        self.unsolvable_corner_parity_test("wbobbbbbbwoooooooogggggggggrrbrrrrrrwwwwwwrwbyyyyyyyyy")
+    def test_unsolvable_corner_parity_4(self):
+        self.unsolvable_corner_parity_test("rbwbbbbbbboooooooogggggggggrrwrrrrrrwwwwwwbwoyyyyyyyyy")
+    def test_unsolvable_corner_parity_5(self):
+        self.unsolvable_corner_parity_test("owgybbbgrwboyoygbowoyrgygowrgyorgbwrggbwwwbroyrwoyrrby")
+    def test_unsolvable_corner_parity_6(self):
+        self.unsolvable_corner_parity_test("owwybbbgroboyoygbowoyrgygowrgyorgbwrggbwwwbrgyrwoyrrby")
+    def test_unsolvable_corner_parity_7(self):
+        self.unsolvable_corner_parity_test("bwgybbbgrwboyoygbowoyrgygowrgoorgbwrggbwwwyroyrwoyrrby")
+    def test_unsolvable_corner_parity_8(self):
+        self.unsolvable_corner_parity_test("ywwybbbgroboyoygbowoyrgygowrgborgbwrggbwwworgyrwoyrrby")
+    
+
+    def unsolvable_permutation_parity_test(self, cube_string):
+        with self.assertRaises(ValueError) as result:
+            rotate(**{'cube':cube_string})
+        self.assertEqual('Error: Cube unsolvable: Permutation parity', str(result.exception))
+    
+    def test_unsolvable_permutation_parity_1(self): # swap edges 1x
+        self.unsolvable_permutation_parity_test("bgbbbbbbbooooooooogbgggggggrrrrrrrrrwwwwwwwwwyyyyyyyyy")
+    def test_unsolvable_permutation_parity_2(self): # swap corners 1x
+        self.unsolvable_permutation_parity_test("obrbbbbbbboooooooogggggggggrrbrrrrrrwwwwwwwwwyyyyyyyyy")
+    def test_unsolvable_permutation_parity_3(self): # swap 3x edges
+        self.unsolvable_permutation_parity_test("brbbbbbbbobooooooogogggggggrgrrrrrrrwwwwwwwwwyyyyyyyyy")
+    def test_unsolvable_permutation_parity_4(self): # swap 2x edges 1x corners
+        self.unsolvable_permutation_parity_test("oorbbbbbbbgooooooogbgggggggrrbrrrrrrwwwwwwwwwyyyyyyyyy")
+    def test_unsolvable_permutation_parity_5(self): # swap edges 1x
+        self.unsolvable_permutation_parity_test("obroboogrwbbwowbbyowwrgrgrorbwyryggbbgwowrgwgyoyyyyygr")
+    def test_unsolvable_permutation_parity_6(self): # swap corners 1x
+        self.unsolvable_permutation_parity_test("wgwoboogrobbwowbbyowwrgrgrorbryryggbbbwowrgwgyoyyyyygr")
+    def test_unsolvable_permutation_parity_7(self): # swap 3x edges
+        self.unsolvable_permutation_parity_test("obroboogrwgbwowbbyobwrgrgrorwwyryggbbrwbwwgogyoyyyyygr")
+    def test_unsolvable_permutation_parity_8(self): # swap 2x edges 1x corners
+        self.unsolvable_permutation_parity_test("wbwoboogrobbwowbbyowwrgrgrorbryryggbbgwrwogwgyoyyyyygr")
 
 if __name__ == '__main__':
     unittest.main()
