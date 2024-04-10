24ed080ecda0c2af96a576bb7246c76a271b420b
Alviere <againes0501@gmail.com>
2024-04-09 19:05:08 -0700

    Solve passing tests

--- rubik_redux/view/solve.py
+++ rubik_redux/view/solve.py
@@ -3,4 +3,13 @@ from rubik_redux.model.cube import Cube
 from rubik_redux.controller import classic_solve
 
 def solve(cube=None, style=None):
-    pass
+    if cube is None:
+        raise ValueError("Error: Input data: Expected parameter \"cube\"")
+    result = {"cube":cube}
+    cube = Cube(cube)
+    match style:
+        case "classic" | None:
+            result["dir"] = classic_solve.solve(cube)
+        case _:
+            raise ValueError("Error: Input data: Could not recognize \"style\" parameter")
+    return result
