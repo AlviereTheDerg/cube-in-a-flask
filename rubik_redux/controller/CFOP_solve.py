7392ae38f634c761e1f4706d95913ac01c573217
Alviere <againes0501@gmail.com>
2024-04-19 17:04:47 -0700

    CFOP easy cases stub and tests

--- rubik_redux/controller/CFOP_solve.py
+++ rubik_redux/controller/CFOP_solve.py
@@ -1,3 +1,7 @@
+"""
+Algorithms as specified in https://ruwix.com/the-rubiks-cube/advanced-cfop-fridrich/
+(With minor modifications)
+"""
 
 import rubik_redux.model.constants as constants
 from rubik_redux.model.cube import Cube
@@ -8,3 +12,17 @@ def solve(cube: Cube):
 from rubik_redux.controller.classic_solve import _bottom_cross
 # TODO: IDENTIFY OPTIMIZATIONS TO REDUCE AVERAGE NUMBER OF MOVES TO SOLVE BOTTOM CROSS
 
+def _first_two_layers_easy_cases(cube: Cube, target: int):
+    pass
+
+def _first_two_layers(cube: Cube):
+    # step 0: repeat following for each 'pillar' (corner of cube, bottom layer corner and middle layer edge)
+    # step 1: locate desired pillar's pieces
+    # step 2: reduce cube entropy:
+    #      2a: pieces are already solved: skip to next pillar
+    #      2b: one or both pieces are in top layer: do nothing for now
+    #      2c: no piece is in top layer, both pieces are in same pillar: do nothing for now
+    #      2d: no piece is in top layer, pieces are in different pillars: raise the corner to the top layer
+    # step 3: reduce cube entropy to an easy-to-solve state
+    # step 4: solve
+    pass
--- rubik_redux/test/CFOP_solveTest.py
+++ rubik_redux/test/CFOP_solveTest.py
@@ -3,6 +3,8 @@ import unittest
 import rubik_redux.controller.CFOP_solve as solver
 from rubik_redux.model.cube import Cube
 from rubik_redux.test.solver_test_skeleton import solver_test_skeleton
+import rubik_redux.model.constants as constants
+from rubik_redux.test.symbol_scrambler import symbol_scramble_decorator
 
 class CFOP_solve_tests(solver_test_skeleton):
     def test_bottom_cross_change_nothing_cross_solved(self):
@@ -44,6 +46,62 @@ class CFOP_solve_tests(solver_test_skeleton):
                      "rllufufrfurbbrldbbuudfbflffrubrlluluffrrubdbbldldddrdd",
                      "urufflfffbuufrurrrfrflbbbbbubbulullllbrlurrflddddddddd"]:
             self.solve_bottom_cross_test(cube)
+
+    # be able to solve the 'easy cases' regardless of top face orientation
+    @symbol_scramble_decorator
+    def solve_first_two_layers_easy_cases_test(self, cube_string, target):
+        cube = Cube(cube_string)
+        for _ in range(4):
+            cube.rotate("U")
+            secondary_cube = Cube(str(cube))
+            result = solver._first_two_layers_easy_cases(secondary_cube, target)
+            self.assertTrue(secondary_cube.match_pattern("...ffffff...rrrrrr...bbbbbb...llllll....u....ddddddddd"), f"While solving target {target}") # solves stage
+            confirm_cube = Cube(str(cube))
+            confirm_cube.rotate(result)
+            self.assertEqual(str(secondary_cube), str(confirm_cube), f"While solving targer {target}") # reproducable
+
+    # in order: FRONT RIGHT, BACK RIGHT, BACK LEFT, FRONT LEFT
+    def test_first_two_layers_help_easy_case__RHS_together(self):
+        self.solve_first_two_layers_easy_cases_test("ffdffuffurrrrrrfrrbllbbbbbbubullllllbuuuufrufddldddddd", constants.DTR)
+        self.solve_first_two_layers_easy_cases_test("ubfffffffurdrrfrrrbbuubbfbbrulllllllbrrluubulddddddddu", constants.DBR)
+        self.solve_first_two_layers_easy_cases_test("furffffffbrrrrrrrruudbbubbllllbllbllblfbuuufuddddddudd", constants.DBL)
+        self.solve_first_two_layers_easy_cases_test("fffrffuffrbfrrrrrruulbbbbbbuudllullrbllfuullubdddddddd", constants.DTL)
+    def test_first_two_layers_help_easy_case__LHS_together(self):
+        self.solve_first_two_layers_easy_cases_test("rffffbfffdllurrlrrurubbbbbbffullllllrubuuubrrddudddddd", constants.DTR)
+        self.solve_first_two_layers_easy_cases_test("urbffffffurrrrbrrfdulubbubbuufllllllblbfublurddddddddr", constants.DBR)
+        self.solve_first_two_layers_easy_cases_test("bbrffffffuuurrrrrrlbbbbrbbbdurullullllffuluufddddddldd", constants.DBL)
+        self.solve_first_two_layers_easy_cases_test("dbflffbffurlrrrrrruuubbbbbbfllllullurfbfuufulrdddddddd", constants.DTL)
+    def test_first_two_layers_help_easy_case__RHS_apart(self):
+        self.solve_first_two_layers_easy_cases_test("ubfffuffrdllfrrfrrurlbbbbbbfrbllllllufbuuururddudddddd", constants.DTR)
+        self.solve_first_two_layers_easy_cases_test("furffffffbbrrrurrldulrbbubbubullllllblbruurfuddddddddf", constants.DBR)
+        self.solve_first_two_layers_easy_cases_test("bluffffffbbfrrrrrrurbbblbbfdurullulllulfuuublddddddrdd", constants.DBL)
+        self.solve_first_two_layers_easy_cases_test("dbfuffuffufbrrrrrrurrbbbbbbuulllfllbfurlulfulldddddddd", constants.DTL)
+    def test_first_two_layers_help_easy_case__LHS_apart(self):
+        self.solve_first_two_layers_easy_cases_test("ffdfflfflrrburrbrrubfbbbbbbufulllllllurruurufddudddddd", constants.DTR)
+        self.solve_first_two_layers_easy_cases_test("rrfffffffurdrrurrlburlbbbbbbbfllllllufruuuublddddddddu", constants.DBR)
+        self.solve_first_two_layers_easy_cases_test("urbfffffflbrrrrrrruudbbubbrlbffllullblfuulluuddddddbdd", constants.DBL)
+        self.solve_first_two_layers_easy_cases_test("frrufflffuuurrrrrrrllbbbbbbuudllbllfbfbfullufudddddddd", constants.DTL)
+    
+    """
+    def test_first_two_layers_unmet_requisite_1(self): # scrambled
+        self.raise_unmet_requisite_stage_test("rbuffbllrlfldrbuurddrrbrdlbdffflulrubrfludulfbubudbddf", solver._first_two_layers, "first two layers")
+    def test_first_two_layers_unmet_requisite_2(self): # scrambled
+        self.raise_unmet_requisite_stage_test("burlfrlbufuudrbbdfblldbudffffdrlfudbubluubrlddrrldfrrl", solver._first_two_layers, "first two layers")
+
+    def test_first_two_layers_change_nothing_first_two_layers_solved_1(self):
+        self.change_nothing_test("fulffffffuurrrrrrrbrubbbbbbfblllllllruuuufulbddddddddd", solver._first_two_layers)
+    def test_first_two_layers_change_nothing_first_two_layers_solved_2(self):
+        self.change_nothing_test("fuuffffffruurrrrrrllbbbbbbblrulllllluufuubrfbddddddddd", solver._first_two_layers)
+    def test_first_two_layers_change_nothing_OLL_solved_1(self):
+        self.change_nothing_test("fbbfffffflrfrrrrrrrlrbbbbbbbfllllllluuuuuuuuuddddddddd", solver._first_two_layers)
+    def test_first_two_layers_change_nothing_OLL_solved_2(self):
+        self.change_nothing_test("rbbffffffllrrrrrrrbflbbbbbbfrflllllluuuuuuuuuddddddddd", solver._first_two_layers)
+    def test_first_two_layers_change_nothing_solved(self):
+        self.change_nothing_test("fffffffffrrrrrrrrrbbbbbbbbbllllllllluuuuuuuuuddddddddd", solver._first_two_layers)
+    
+    def solve_first_two_layers_test(self, cube_string):
+        self.solve_successful_test(cube_string, solver._first_two_layers, "...ffffff...rrrrrr...bbbbbb...llllll....u....ddddddddd")
+    """
 
 if __name__ == '__main__':
     unittest.main()
